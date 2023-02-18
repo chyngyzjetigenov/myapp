4f074f33dceae4354035e9e8b2af0c2b777ca77a
chyngyzjetigenov <117899294+chyngyzjetigenov@users.noreply.github.com>
2023-02-18 11:39:42 +0600

    Create entrypoint.sh

---
+++ .github/actions/deploy/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/shmkdir ~/.sshecho $VAULT_PASS > /vault_password_file.txtansible-vault decrypt \
+--vault-password-file=/vault_password_file.txt \
+id_rsa-encrypted \
+--output=~/.ssh/id_rsachmod 0600 ~/.ssh/id_rsaansible-playbook --vault-password-file=/vault_password_file.txt \
+-i ansible/hosts \
+ansible/deploy.yml
